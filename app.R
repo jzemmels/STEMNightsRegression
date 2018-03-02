373578d0dfd893b5f68fa4b4654720c7c5ab8e5d
Nehemias Ulloa <nulloa1@iastate.edu>
2018-03-02 16:02:06 -0600

    Add test data and fix grade

--- app.R
+++ app.R
@@ -11,6 +11,7 @@
 #==================================
 require(shiny)
 require(ggplot2)
+require(dplyr)
 require(plyr)
 require(plotly)
 
@@ -20,11 +21,11 @@ shinyUI <- fluidPage(
   titlePanel("Hello Kids!"),
   sidebarLayout(
     sidebarPanel(width = 4,
-      numericInput("height", label = h3("Height (inches)"), value = 1),
-      numericInput("shoe", label = h3("Shoe Size (inches"), value = 1),
+      numericInput("height", label = h3("Height (in)"), value = 1),
+      numericInput("shoe", label = h3("Shoe Size (in)"), value = 1),
       selectInput("grade", label = h3("Grade"), 
-                  choices = list("Kindergarten" = 'K', "1st" = 1, "2nd" = 2, "3rd" = 3, "4th" = 4,
-                                 "5th" = 5, "6th" = 6, "Adult" = 7), selected = 'K'),
+                  choices = list("Kindergarten" = 'K', "1st" = "1", "2nd" = "2", "3rd" = "3", "4th" = "4",
+                                 "5th" = "5", "6th" = "6", "Adult" = "7"), selected = 'K'),
       selectInput("gender", label = h3("Gender"),  choices = c('F', 'M')),
       actionButton("goButton", "Go!", class = "btn-primary")
     ),
@@ -53,7 +54,7 @@ shinyServer <- function(input, output){
     input$goButton
     d <- read.csv("height_shoesizeData.csv", header=TRUE)
     d$Grade <- factor(d$Grade)
-    d$Grade <- revalue(d$Grade, c("0"="Kindergarten","1"="1st","2"="2nd","3"="3rd","4"="4th","5"="5th","6"="6th","7"="Adult"))
+    d$Grade <- revalue(d$Grade, c("K"="Kindergarten","1"="1st","2"="2nd","3"="3rd","4"="4th","5"="5th","6"="6th","7"="Adult"))
     d[1:5, ]
   })
     
@@ -61,21 +62,21 @@ shinyServer <- function(input, output){
     input$goButton
     d <- read.csv("height_shoesizeData.csv", header=TRUE)
     d$Grade <- factor(d$Grade)
-    d$Grade <- revalue(d$Grade, c("0"="Kindergarten","1"="1st","2"="2nd","3"="3rd","4"="4th","5"="5th","6"="6th","7"="Adult"))
+    d$Grade <- revalue(d$Grade, c("K"="Kindergarten","1"="1st","2"="2nd","3"="3rd","4"="4th","5"="5th","6"="6th","7"="Adult"))
     d$Gender <- factor(d$Gender)
     d$Gender <- revalue(d$Gender, c("F"="GIRL","M"="BOY"))
     
     # draw the histogram with the specified number of bins
    p<- ggplot(data=d, aes(x=Height, y=ShoeSize), size=I(1)) + 
      geom_point() +  geom_smooth(method = "lm",se = FALSE) + 
-     geom_point(aes(colour=factor(Grade))) +
+     geom_point(aes(colour=Grade)) +
      #geom_point(data=d[1,], aes(x=Height, y=ShoeSize), size=I(2), color=I('red')) +
      facet_grid(Gender ~ .) + 
-      labs(x="Height", y="Shoe Size") + 
-      theme(aspect.ratio = 1, 
-            axis.title.y = element_text(size = rel(1.8), angle = 90), 
-            axis.title.x = element_text(size = rel(1.8), angle = 00))
-  ggplotly(p)
+      labs(x="Height", y="Shoe Size")
+      #theme(aspect.ratio = 1, 
+            # axis.title.y = element_text(size = rel(1.8), angle = 90), 
+            # axis.title.x = element_text(size = rel(1.8), angle = 00))
+  ggplotly(p, width = 800, height = 600)
    })
   
 }
